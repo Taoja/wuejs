13ec31510f3ae8e6d0ab530bca2a39ff262acf8f
huangwutao <346792184@qq.com>
2019-02-22 11:48:26 +0800

    增加自动化构建晚上项目配置

---
+++ build/rollup.js
@@ -0,0 +1,36 @@
+const rollup = require('rollup')
+const commonjs = require('rollup-plugin-commonjs')
+const resolve = require('rollup-plugin-node-resolve')
+const uglify = require('rollup-plugin-uglify-es')
+const license = require('rollup-plugin-license')
+const args = process.argv
+
+const format = args.includes('es') ? 'es' : 'umd'
+
+rollup.rollup({
+  input: 'src/index.js',
+  plugins: [
+    commonjs(),
+    resolve(),
+    license({
+      banner: `
+        Wue v<%= pkg.version %>
+        (c) 2018-<%= moment().format('YYYY') %> huangwutao
+        Released under the MIT @License.
+        document -> https://taoja.github.io/wue
+        github -> https://github.com/Taoja/wue
+      `
+    }),
+    uglify({
+      output: {
+        comments: 'some'
+      }
+    })
+  ]
+}).then(function (bundle) {
+  bundle.write({
+    name: 'Wue',
+    format: format,
+    file: `dist/wue.${format == 'umd' ? 'min' : format}.js`,
+  })
+})
